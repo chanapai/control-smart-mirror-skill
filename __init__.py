883ac479637e8510fb1b6625b2c99b7974909bec
Chanapai <chanapai.limsuwan@gmail.com>
2020-11-26 14:41:31 +0700

    test intent and entity

--- __init__.py
+++ __init__.py
@@ -5,11 +5,23 @@ class ControlSmartMirror(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+"""
     @intent_file_handler('mirror.smart.control.intent')
     def handle_mirror_smart_control(self, message):
-        self.speak_dialog('mirror.smart.control')
+        self.speak_dialog('mirror.smart.control')"""
 
+    @intent_handler('turn.on.intent')
+    def handle_turn_on_intent(self, message):
+        self.log.debug("Turn on intent on entity: "+message.data.get("entity"))
+        #module = message.data.get("entity")
+       
 
+    @intent_handler('turn.off.intent')
+    def handle_turn_off_intent(self, message):
+         self.log.debug("Turn of intent on entity: "+message.data.get("entity"))
+        # module = message.data.get("entity")
+
+    
 def create_skill():
     return ControlSmartMirror()
 
