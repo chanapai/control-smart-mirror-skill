6655fe009d2ed9512f12b862efccab9fa5f1a4cf
Chanapai <chanapai.limsuwan@gmail.com>
2020-12-01 08:42:57 +0700

    edit intent

--- __init__.py
+++ __init__.py
@@ -15,16 +15,22 @@ class ControlSmartMirror(MycroftSkill):
 
     @intent_handler('hide.intent')
     def handle_turn_on_intent(self, message):
-        self.log.debug("Turn on intent on entity: "+message.data.get("entity"))
+        #self.log.debug("Turn on intent on entity: "+message.data.get("entity"))
         self.speak_dialog("done.dialog")
         #module = message.data.get("entity")
        
 
     @intent_handler('show.intent')
     def handle_turn_off_intent(self, message):
-         self.log.debug("Turn of intent on entity: "+message.data.get("entity"))
+         #self.log.debug("Turn of intent on entity: "+message.data.get("entity"))
          self.speak_dialog("done.dialog")
         # module = message.data.get("entity")
+
+
+    @intent_handler(IntentBuilder('HelloSmartMirror').require('HelloSmartMirror'))
+    def handle_hello_smart_mirror_intent(self, message):
+        self.speak_dialog("done.dialog")
+
 
     def stop(self):
         pass
