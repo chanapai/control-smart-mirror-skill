326763193f1ff8b2e72ab6d88542c6df7945898b
Chanapai <chanapai.limsuwan@gmail.com>
2020-12-01 08:56:50 +0700

    edit intent

--- __init__.py
+++ __init__.py
@@ -8,17 +8,17 @@ class ControlSmartMirror(MycroftSkill):
 
     def initialize(self):
         my_setting = self.settings.get('my_setting')
-"""
+    """
     @intent_file_handler('mirror.smart.control.intent')
     def handle_mirror_smart_control(self, message):
         self.speak_dialog('mirror.smart.control')"""
 
-    @intent_handler('hide.intent')
+    '''@intent_handler('hide.intent')
     def handle_turn_on_intent(self, message):
         #self.log.debug("Turn on intent on entity: "+message.data.get("entity"))
         self.speak_dialog("done.dialog")
         #module = message.data.get("entity")
-       
+    '''
 
     @intent_handler('show.intent')
     def handle_turn_off_intent(self, message):
